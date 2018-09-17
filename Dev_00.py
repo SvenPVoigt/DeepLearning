9921522edf2a326ad1345f38c7d501c25466c2c9
Sven <svoigt6@gatech.edu>
2018-09-17 16:52:09 -0400

    first CNN steps

---
+++ Dev_00.py
@@ -0,0 +1,9 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+
+import matplotlib.pyplot as plt
+
+p
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a test repository where I can learn about learning.
